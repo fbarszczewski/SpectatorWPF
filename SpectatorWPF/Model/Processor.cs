e946a59e94e022254e939030497b08a6c4f32de3
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-05 19:22:15 +0100

    Dodaj pliki projektów.

---
+++ SpectatorWPF/Model/Processor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectatorWPF.Model
+{
+    internal class Processor
+    {
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public string Architecture { get; set; }
+
+        public string NumberOfCores { get; }
+
+        public string CurrentClockSpeed { get; }
+
+    }
+}
---
+++ SpectatorWPF/Model/Wmi.cs
@@ -0,0 +1,36 @@
+﻿using System.Management;
+
+namespace SpectatorWPF.Model
+{
+    internal class Wmi
+    {
+        private  ManagementObjectSearcher _searcher;
+
+        public Wmi()
+        {
+            _searcher = new ManagementObjectSearcher();
+        }
+
+
+        public string GetValue(string fromNamespace, string fromClass, string  property )
+        {
+            var propertyValue = "";
+            try
+            {
+                foreach (var queryObj in new ManagementObjectSearcher(fromNamespace, $"SELECT {property} FROM {fromClass}").Get())
+                {
+                    propertyValue = queryObj[property].ToString();
+                }
+
+            }
+            catch (ManagementException e)
+            {
+                propertyValue = e.Message;
+            }
+
+            return propertyValue;
+
+        }
+        // _searcher = new ManagementObjectSearcher("root\\WMI", "SELECT * FROM MSWmi_PnPInstanceNames");
+    }
+}
