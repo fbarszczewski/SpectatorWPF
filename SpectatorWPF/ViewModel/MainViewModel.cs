860b6454b4a1819d61da9d7436e4b1f9d06d19ad
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-08 14:41:05 +0100

    device view CD.

---
+++ SpectatorWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpectatorWPF.Core;
+
+namespace SpectatorWPF.ViewModel
+{
+    class MainViewModel :ObservableObject
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public DeviceViewModel DeviceVM { get; set; }
+
+        public MainViewModel()
+        {
+            DeviceVM = new DeviceViewModel();
+            CurrentView = DeviceVM;
+        }
+    }
+}
